938bb5b9f5de6b26866f5724a6e683c732b0a633
MAHA <mem880.a@gmail.com>
2024-08-21 01:23:57 +0300

    Modifing main file

--- lib/screens/credit_card_screen.dart
+++ lib/screens/credit_card_screen.dart
@@ -275,27 +275,3 @@ class _CreditCardScreenState extends State<CreditCardScreen> {
   }
 }
 
-class ExpiryDateFormatter {
-  TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
-    var text = newValue.text;
-
-    if (newValue.selection.baseOffset == 0) {
-      return newValue;
-    }
-
-    var buffer = StringBuffer();
-    for (int i = 0; i < text.length; i++) {
-      buffer.write(text[i]);
-      if (i == 1 && text.length < 3) {
-        buffer.write('/');
-      }
-    }
-
-    var string = buffer.toString();
-    return newValue.copyWith(
-      text: string,
-      selection: TextSelection.collapsed(offset: string.length),
-    );
-  }
-}
