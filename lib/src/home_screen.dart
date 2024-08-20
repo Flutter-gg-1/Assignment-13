1f1558688e5f2d3dae08aac99766c238dd86f162
Amjad Elahi <amjadelahi07@gmail.com>
2024-08-20 21:03:15 +0300

    implements home screen

---
+++ lib/src/home_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController cardNumberCotroller = TextEditingController();
+    TextEditingController cardHolderCotroller = TextEditingController();
+    TextEditingController expiredDateMCotroller = TextEditingController();
+    TextEditingController expiredDateYCotroller = TextEditingController();
+    return Scaffold(
+      backgroundColor: const Color(0xffcbd0dc),
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              height: 350,
+              width: 315.48,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.white,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 31, right: 31),
+                    child: TextFormField(
+                      controller: cardNumberCotroller,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter card number";
+                        }
+                        if (value.length != 16) {
+                          return "The card number consist of 16 digits";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          hintText: "Card number",
+                          hintStyle: const TextStyle(
+                              color: Color(0xffC9CBD0), fontSize: 14),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(3.28))),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 31, vertical: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            controller: expiredDateMCotroller,
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter the month of expired date ";
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                                hintText: "Expired date (MM)",
+                                hintStyle: const TextStyle(
+                                    color: Color(0xffC9CBD0), fontSize: 9),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(3.6))),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        Expanded(
+                          child: TextFormField(
+                            controller: expiredDateYCotroller,
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter the year of expired date ";
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                                hintText: "Expired date (YY)",
+                                hintStyle: const TextStyle(
+                                    color: Color(0xffC9CBD0), fontSize: 9),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(3.6))),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 31, right: 31, top: 40),
+                    child: TextFormField(
+                      controller: cardHolderCotroller,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      keyboardType: TextInputType.text,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter card holder name";
+                        }
+                        if (RegExp(r'[0-9]').hasMatch(value)) {
+                          return "The card holder name consist of letters only!";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          hintText: "Card Holder",
+                          hintStyle: const TextStyle(
+                              color: Color(0xffC9CBD0), fontSize: 14),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(3.28))),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(top: 137),
+              child: Container(
+                height: 53.11,
+                width: 269.46,
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                        colors: [Color(0xff0B80F3), Color(0xff0C67C0)],
+                        begin: Alignment(0, 0)),
+                    borderRadius: BorderRadius.circular(15.62),
+                    border: Border.all(color: Colors.white, width: 1)),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if(_formKey.currentState?.validate() ?? false){
+                      
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.62),
+                    ),
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.transparent,
+                  ),
+                  child: const Text(
+                    "Show card data",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ))
+        ],
+      )),
+    );
+  }
+}
