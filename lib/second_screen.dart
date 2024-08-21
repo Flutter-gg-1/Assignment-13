50339480555a33e624e68baa83c0f3ce4f5f9001
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-21 07:34:51 +0300

    UPD Good 2

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -24,24 +24,56 @@ class _SecondScreenState extends State<SecondScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       // show the card details
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Center(
+      body: Stack(
+        children: [
+          Container(
+            color: const Color.fromRGBO(203, 208, 220, 1),
+            width: double.infinity,
+            height: double.infinity,
+            child: Center(
               child: Container(
                   color: const Color.fromRGBO(155, 150, 150, 1),
                   width: 320,
                   height: 183,
                   child: Column(
                     children: [
+                      const SizedBox(height: 70),
                       myTextFormField(
                         enabled: false,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 150,
+                            height: 50,
+                            child: myTextFormField(
+                              enabled: false,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 90,
+                            height: 50,
+                            child: myTextFormField(
+                              enabled: false,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 70,
+                            height: 50,
+                            child: myTextFormField(
+                              enabled: false,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   )),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
