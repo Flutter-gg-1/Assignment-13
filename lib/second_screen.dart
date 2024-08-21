bb99f47a7628e61915af8263dc4075bfb17bf9ae
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-21 08:05:19 +0300

    UPD Good 5git add .

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -25,66 +25,90 @@ class _SecondScreenState extends State<SecondScreen> {
     return Scaffold(
       // show the card details
       body: Container(
-        color: const Color.fromRGBO(203, 208, 220, 1),
+        color: const Color.fromRGBO(213, 214, 218, 1),
         width: double.infinity,
         height: double.infinity,
         child: Center(
           child: Stack(
             children: [
-              Container(
-                  color: const Color.fromRGBO(155, 150, 150, 1),
-                  width: 320,
-                  height: 183,
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 70),
-                      myTextFormField(
-                        enabled: false,
-                        labelText: widget.cardNumber,
-                        fontSizeLabel: 20,
-                        colorLabel: Colors.white,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: [
-                          SizedBox(
-                            width: 150,
-                            height: 50,
-                            child: myTextFormField(
-                              enabled: false,
-                              labelText: widget.cardHolderName,
-                              fontSizeLabel: 16,
-                              colorLabel: Colors.white,
+              Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromRGBO(155, 150, 150, 1),
+                    ),
+                    width: 320,
+                    height: 183,
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 70),
+                        myTextFormField(
+                          enabled: false,
+                          labelText: widget.cardNumber,
+                          fontSizeLabel: 20,
+                          colorLabel: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              height: 50,
+                              child: myTextFormField(
+                                enabled: false,
+                                labelText: widget.cardHolderName,
+                                fontSizeLabel: 16,
+                                colorLabel: Colors.white,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            width: 90,
-                            height: 50,
-                            child: myTextFormField(
-                              enabled: false,
-                              labelText: 'VALID THRU',
-                              fontSizeLabel: 10,
-                              fontSize: 10,
-                              colorLabel: Colors.white,
+                            SizedBox(
+                              width: 90,
+                              height: 50,
+                              child: myTextFormField(
+                                enabled: false,
+                                labelText: 'VALID THRU',
+                                fontSizeLabel: 10,
+                                fontSize: 10,
+                                colorLabel: Colors.white,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            width: 70,
-                            height: 50,
-                            child: myTextFormField(
-                              enabled: false,
-                              labelText:
-                                  '${widget.expiryMonthDate1}/${widget.expiryYearDate1}',
-                              fontSizeLabel: 14,
-                              colorLabel: Colors.white,
+                            SizedBox(
+                              width: 70,
+                              height: 50,
+                              child: myTextFormField(
+                                enabled: false,
+                                labelText:
+                                    '${widget.expiryMonthDate1}/${widget.expiryYearDate1}',
+                                fontSizeLabel: 14,
+                                colorLabel: Colors.white,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  )),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Positioned(
+                    top: -70,
+                    right: 250,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.yellowAccent,
+                      radius: 80,
+                    ),
+                  ),
+                  const Positioned(
+                    top: -70,
+                    right: 260,
+                    child: CircleAvatar(
+                      backgroundColor: Color.fromRGBO(155, 150, 150, 1),
+                      radius: 70,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
