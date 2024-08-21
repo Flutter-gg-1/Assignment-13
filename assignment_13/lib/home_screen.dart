144a0c6074895fcbf684e010f0c3b19ea81526c5
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-21 08:31:34 +0300

    Done git add .git add .git add .!

---
+++ assignment_13/lib/home_screen.dart
@@ -0,0 +1,207 @@
+import 'package:assignment_13/common.dart';
+import 'package:assignment_13/second_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController myControllerName = TextEditingController();
+  final TextEditingController myControllerNumber = TextEditingController();
+  final TextEditingController myControllerMonthDate = TextEditingController();
+  final TextEditingController myControllerYearDate = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String cardNumber = '';
+  String expiryMonthDate = '';
+  String expiryYearDate = '';
+  String cardHolderName = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              color: const Color.fromRGBO(203, 208, 220, 1),
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 318,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          myTextFormField(
+                              controller: myControllerNumber,
+                              maxLength: 16,
+                              hintText: 'Card number',
+                              fontSize: 14,
+                              fontSizeForInput: 14,
+                              onSaved: (p0) {
+                                cardNumber = p0.toString();
+                              },
+                              inputFormatters: [
+                                FilteringTextInputFormatter.allow(
+                                    RegExp(r'[0-9]')),
+                              ],
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter card number';
+                                } else if (value.length < 16) {
+                                  return 'Please enter valid card number';
+                                } else {
+                                  return null;
+                                }
+                              }),
+                          const SizedBox(height: 5),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              SizedBox(
+                                width: 115,
+                                height: 40,
+                                child: myTextFormField(
+                                    controller: myControllerMonthDate,
+                                    maxLength: 2,
+                                    hintText: 'Expired date (MM)',
+                                    fontSize: 10,
+                                    fontSizeForInput: 10,
+                                    onSaved: (p0) {
+                                      expiryMonthDate = p0.toString();
+                                    },
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.allow(
+                                          RegExp(r'[0-9]')),
+                                    ],
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Invalid';
+                                      } else if (value.length < 2) {
+                                        return 'Invalid';
+                                      } else {
+                                        return null;
+                                      }
+                                    }),
+                              ),
+                              SizedBox(
+                                width: 115,
+                                height: 40,
+                                child: myTextFormField(
+                                    controller: myControllerYearDate,
+                                    maxLength: 2,
+                                    hintText: 'Expired date (YY)',
+                                    fontSize: 10,
+                                    fontSizeForInput: 10,
+                                    onSaved: (p0) {
+                                      expiryYearDate = p0.toString();
+                                    },
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.allow(
+                                          RegExp(r'[0-9]')),
+                                    ],
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Invalid';
+                                      } else if (value.length < 2) {
+                                        return 'Invalid';
+                                      } else {
+                                        return null;
+                                      }
+                                    }),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 5),
+                          myTextFormField(
+                              controller: myControllerName,
+                              hintText: 'Card Holder',
+                              fontSize: 14,
+                              fontSizeForInput: 14,
+                              onSaved: (p0) {
+                                cardHolderName = p0.toString();
+                              },
+                              inputFormatters: [
+                                FilteringTextInputFormatter.allow(
+                                    RegExp(r'[a-zA-Z]')),
+                              ],
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your name';
+                                } else if (value.length < 3) {
+                                  return 'Please enter valid name';
+                                } else {
+                                  return null;
+                                }
+                              }),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 130),
+                  SizedBox(
+                    width: 270,
+                    height: 54,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: const WidgetStatePropertyAll<Color>(
+                            Color.fromRGBO(11, 128, 243, 1)),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SecondScreen(
+                                        cardNumber: cardNumber,
+                                        expiryMonthDate1: expiryMonthDate,
+                                        expiryYearDate1: expiryYearDate,
+                                        cardHolderName: cardHolderName,
+                                      )));
+                          myControllerName.clear();
+                          myControllerNumber.clear();
+                          myControllerMonthDate.clear();
+                          myControllerYearDate.clear();
+                        } else {
+                          print('Error');
+                        }
+                      },
+                      child: const Text(
+                        'Show card data',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13.5,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
